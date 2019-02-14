4394eeeab132b1554d5a9d3ccdadfa2e3d0f378b
AnoukvanVugt <delephos@gmail.com>
2019-02-14 12:05:09 +0100

    Perfect Pom File Updated

---
+++ perfect-pom/src/main/java/nl/han/ica/dea/anouk/App.java
@@ -0,0 +1,13 @@
+package nl.han.ica.dea.anouk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
